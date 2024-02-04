83147f259cccca782f3c695151aab7531742aee4
jj <jotajota1302@hotmail.com>
2024-02-04 09:17:07 +0100

    commit

---
+++ ico-validation/src/main/java/com/ntt/es/config/Constantes.java
@@ -0,0 +1,21 @@
+package com.ntt.es.config;
+
+
+public abstract class Constantes {
+	
+	public static final String LINEA_ICO_MRR_PROMOCION_DE_VIVIENDA_SOCIAL = "ICO MRR Promoción de vivienda social";
+	public static final String LINEA_ICO_MRR_AUDIOVISUAL_PERTE_NUEVA_ECONOMIA_DE_LA_LENGUA = "ICO MRR Audiovisual – PERTE Nueva Economía de la Lengua";
+	public static final String LINEA_ICO_MRR_AUDIOVISUAL = "ICO MRR Audiovisual";
+	public static final String LINEA_ICO_MRR_EMPRESAS_Y_EMPRENDEDORES_PERTE_NUEVA_ECONOMIA_DE_LA_LENGUA = "ICO MRR Empresas y Emprendedores – PERTE Nueva Economía de la Lengua";
+	public static final String LINEA_ICO_MRR_EMPRESAS_Y_EMPRENDEDORES_SECTOR_TURISTICO = "ICO MRR Empresas y Emprendedores – Sector turístico";
+	public static final String LINEA_ICO_MRR_EMPRESAS_Y_EMPRENDEDORES = "ICO MRR Empresas y Emprendedores";
+	public static final String LINEA_ICO_MRR_VERDE_PERTE_ENERGIA_RENOVABLE_HIDROGENO_Y_ALMACENAMIENTO = "ICO MRR Verde – PERTE Energía Renovable, Hidrógeno y Almacenamiento";
+	public static final String LINEA_ICO_MRR_VERDE = "ICO MRR Verde";
+	public static final String EMPRESA_PUBLICA = "Empresa pública";
+	public static final String UNIVERSIDAD_ESPANOLA = "Universidad española";
+	public static final String HOGAR = "Hogar";
+	public static final String AUTONOMO = "Autónomo";
+	public static final String EMPRESA_PRIVADA = "Empresa privada";
+
+
+}
--- ico-validation/src/main/java/com/ntt/es/validation/validator/TipoClienteValidator.java
+++ ico-validation/src/main/java/com/ntt/es/validation/validator/TipoClienteValidator.java
@@ -5,70 +5,65 @@ import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.ntt.es.config.Constantes;
 import com.ntt.es.model.DatosTitularesDto;
 import com.ntt.es.validation.annotations.ValidarTipoCliente;
 
 public class TipoClienteValidator implements ConstraintValidator<ValidarTipoCliente, DatosTitularesDto> {
 
-	private static final String EMPRESA_PUBLICA = "Empresa pública";
-	private static final String UNIVERSIDAD_ESPANOLA = "Universidad española";
-	private static final String HOGAR = "Hogar";
-	private static final String AUTONOMO = "Autónomo";
-	private static final String EMPRESA_PRIVADA = "Empresa privada";
-
+	
 	@Autowired
 	EmpresaPrivadaValidator empresaPrivadaValidator;
-	
+
 	@Autowired
 	EmpresaPublicaValidator empresaPublicaValidator;
-	
+
 	@Override
 	public boolean isValid(DatosTitularesDto dto, ConstraintValidatorContext context) {
 
-		boolean isValid=true;
-		
-		// Verificar el tipo de línea seleccionada
-		isValid=validaLineaTipoCliente(dto, dto.getLinea());
+		boolean isValid = true;
 
-		if(!isValid) {
+		// Verificar el tipo de línea seleccionada
+		isValid = validaLineaTipoCliente(dto, dto.getLinea());
+
+		if (!isValid) {
 			context.disableDefaultConstraintViolation();
-			 context.buildConstraintViolationWithTemplate("La linea no coincide con el tipo de cliente.")
-             .addPropertyNode("razonSocialTitular")
-             .addConstraintViolation();
-		}	       
-		
-		if (EMPRESA_PRIVADA.equals( dto.getTipoCliente())) {			
-	            if (!empresaPrivadaValidator.isValid(dto, context)) {
-	                isValid=false;
-	            }
-		}		
-		if (EMPRESA_PUBLICA.equals( dto.getTipoCliente())) {		
-	            if (!empresaPublicaValidator.isValid(dto, context)) {
-	            	isValid=false;
-	            }
+			context.buildConstraintViolationWithTemplate("La linea no coincide con el tipo de cliente.")
+					.addPropertyNode("razonSocialTitular").addConstraintViolation();
 		}
-		
+
+		if (Constantes.EMPRESA_PRIVADA.equals(dto.getTipoCliente())) {
+			if (!empresaPrivadaValidator.isValid(dto, context)) {
+				isValid = false;
+			}
+		}
+		if (Constantes.EMPRESA_PUBLICA.equals(dto.getTipoCliente())) {
+			if (!empresaPublicaValidator.isValid(dto, context)) {
+				isValid = false;
+			}
+		}
+
 		return isValid;
 	}
 
 	private boolean validaLineaTipoCliente(DatosTitularesDto dto, String lineaSeleccionada) {
 
 		// Validar el tipo de cliente según la línea seleccionada
-		
-		if ("ICO MRR Verde".equals(lineaSeleccionada)
-				|| "ICO MRR Verde – PERTE Energía Renovable, Hidrógeno y Almacenamiento".equals(lineaSeleccionada)) {
-			return esTipoClienteValido(dto.getTipoCliente(), new String[] { EMPRESA_PRIVADA, AUTONOMO, HOGAR });
-		} else if ("ICO MRR Empresas y Emprendedores".equals(lineaSeleccionada)
-				|| "ICO MRR Empresas y Emprendedores – Sector turístico".equals(lineaSeleccionada)) {
-			return esTipoClienteValido(dto.getTipoCliente(), new String[] { EMPRESA_PRIVADA, AUTONOMO });
-		} else if ("ICO MRR Empresas y Emprendedores – PERTE Nueva Economía de la Lengua".equals(lineaSeleccionada)) {
-			return esTipoClienteValido(dto.getTipoCliente(), new String[] { UNIVERSIDAD_ESPANOLA });
-		} else if ("ICO MRR Audiovisual".equals(lineaSeleccionada)
-				|| "ICO MRR Audiovisual – PERTE Nueva Economía de la Lengua".equals(lineaSeleccionada)) {
+
+		if (Constantes.LINEA_ICO_MRR_VERDE.equals(lineaSeleccionada)
+				|| Constantes.LINEA_ICO_MRR_VERDE_PERTE_ENERGIA_RENOVABLE_HIDROGENO_Y_ALMACENAMIENTO.equals(lineaSeleccionada)) {
+			return esTipoClienteValido(dto.getTipoCliente(), new String[] { Constantes.EMPRESA_PRIVADA, Constantes.AUTONOMO, Constantes.HOGAR });
+		} else if (Constantes.LINEA_ICO_MRR_EMPRESAS_Y_EMPRENDEDORES.equals(lineaSeleccionada)
+				|| Constantes.LINEA_ICO_MRR_EMPRESAS_Y_EMPRENDEDORES_SECTOR_TURISTICO.equals(lineaSeleccionada)) {
+			return esTipoClienteValido(dto.getTipoCliente(), new String[] { Constantes.EMPRESA_PRIVADA, Constantes.AUTONOMO });
+		} else if (Constantes.LINEA_ICO_MRR_EMPRESAS_Y_EMPRENDEDORES_PERTE_NUEVA_ECONOMIA_DE_LA_LENGUA.equals(lineaSeleccionada)) {
+			return esTipoClienteValido(dto.getTipoCliente(), new String[] { Constantes.UNIVERSIDAD_ESPANOLA });
+		} else if (Constantes.LINEA_ICO_MRR_AUDIOVISUAL.equals(lineaSeleccionada)
+				|| Constantes.LINEA_ICO_MRR_AUDIOVISUAL_PERTE_NUEVA_ECONOMIA_DE_LA_LENGUA.equals(lineaSeleccionada)) {
 			return esTipoClienteValido(dto.getTipoCliente(),
-					new String[] { EMPRESA_PRIVADA, EMPRESA_PUBLICA, AUTONOMO });
-		} else if ("ICO MRR Promoción de vivienda social".equals(lineaSeleccionada)) {
-			return esTipoClienteValido(dto.getTipoCliente(), new String[] { EMPRESA_PRIVADA, EMPRESA_PUBLICA });
+					new String[] { Constantes.EMPRESA_PRIVADA, Constantes.EMPRESA_PUBLICA, Constantes.AUTONOMO });
+		} else if (Constantes.LINEA_ICO_MRR_PROMOCION_DE_VIVIENDA_SOCIAL.equals(lineaSeleccionada)) {
+			return esTipoClienteValido(dto.getTipoCliente(), new String[] { Constantes.EMPRESA_PRIVADA, Constantes.EMPRESA_PUBLICA });
 		}
 
 		return false;
@@ -80,7 +75,7 @@ public class TipoClienteValidator implements ConstraintValidator<ValidarTipoClie
 				return true;
 			}
 		}
-				
+
 		return false;
 	}
 
