5466cfb7606a698a3843dc6c01be1645d6ab7100
jj <jotajota1302@hotmail.com>
2024-02-04 19:49:11 +0100

    fix

--- ico-validation/src/main/java/com/ntt/es/model/dto/DatosFinancierosDto.java
+++ ico-validation/src/main/java/com/ntt/es/model/dto/DatosFinancierosDto.java
@@ -45,15 +45,11 @@ public class DatosFinancierosDto {
     private List<DatosFinancierosNoGrupoDto> datosEmpresasNoGrupo;
     
     private List<DatosFinancierosAutonomoDto> datosFinancierosAutonomo;    
-
-    @NotBlank(message = "La página web es obligatoria. En caso de no tener página web, utilizar 'NA'.")
+   
     private String paginaWeb;
 
-    @NotBlank(message = "El correo electrónico de la persona de contacto es obligatorio. En caso de no tener correo electrónico, utilizar 'NA'.")
-    @Pattern(regexp = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}\\b|NA", message = "Ingrese una dirección de correo electrónico válida o utilice 'NA'.")
     private String correoPersonaContacto;
 
-    @NotBlank(message = "El código CNAE cliente es obligatorio.")
     private String codigoCNAECliente;
     
 }
